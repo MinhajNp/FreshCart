b273273d69c3fc6d47918aeecbccd7225d22c5db
MinhajNp <minhajmohammednp@gmail.com>
2025-04-18 17:56:08 +0530

    added product category page

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,9 +4,9 @@ import { useAppContext } from "../context/AppContext";
 
 
 const ProductCard = ({product}) => {
-    const [count, setCount] = React.useState(0);
+    
     const {currency, addToCart, removeFromCart, cartItems, navigate} = useAppContext()
-    console.log("productCard", cartItems)
+    
 
    
     return product && (
