0ebd86a30b7ccbe317659b66a1a4434f089aa578
MinhajNp <minhajmohammednp@gmail.com>
2025-04-22 17:54:42 +0530

    added product apis and cart apis

---
+++ server/routes/cartRoute.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { updateCart } from '../controllers/cartController.js';
+
+const cartRouter = mongoose.Router();
+
+cartRouter.post('/update', authUser, updateCart)
+
+export default cartRouter;
