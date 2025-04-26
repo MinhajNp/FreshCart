0cc1efdc575938b160ebc56b136967ea3a33d25b
MinhajNp <minhajmohammednp@gmail.com>
2025-04-26 09:24:08 +0530

    updated a bug

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -4,7 +4,8 @@ import User from "../models/User.js"
 // update User cartData : /api/cart/update
 export const updateCart = async (req, res)=>{
     try {
-        const { userId, cartItems } = req.body
+        const { cartItems } = req.body
+        const userId = req.userId;
         await User.findByIdAndUpdate(userId, {cartItems})
         res.json({success: true, message: "Cart Updated"})
     } catch (error) {
